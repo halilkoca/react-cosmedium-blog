1b1e4ffe680000a3a520a56c98f05503b3ba51fe
Halil Koca <ihalilkoca@gmail.com>
2022-02-25 01:37:45 +0300

    first commit

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -4,10 +4,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="footer">
- 
-    <p>Powered by <a href="https://cosmicjs.com">CosmicJs</a> created by <a href="https://github.com/3ndrius">3ndrius</a> Inspired by <a href="https://medium.com/">Medium</a> </p>
-    
-     
+      <p>Powered by CosmicJs. Forked & Created by Halil Koca. Inspired by Medium </p>
     </div>
   )
 }
--- src/components/Category.js
+++ src/components/Category.js
@@ -4,42 +4,42 @@ import { Link } from 'react-router-dom';
 
 import '../css/Category.css';
 
- function Category(props) {
-    
+function Category(props) {
+
   return (
     <div>
-       {
-          props.postCategory &&  props.postCategory.map((item, index) => {
-            return(
+      {
+        props.postCategory && props.postCategory.map((item, index) => {
+          return (
             <div key={index} className="main-category-wrap">
-            {item && <img className="category-image" src={item.metadata.image.url} alt={item.slug}/> }
-           <div className="category-text-wrap">
-           <Link to={'/' + item.slug}> <h2>{item.title}</h2></Link> 
-           { item.length < 300 ? 
-           <p dangerouslySetInnerHTML={{__html:item.content}}></p> :
-           <p dangerouslySetInnerHTML={{__html:item.content.substring(0, 300)}}></p> 
-           }
-           </div>
+              {item && <img className="category-image" src={item.metadata.image.url} alt={item.slug} />}
+              <div className="category-text-wrap">
+                <Link to={'/' + item.slug}> <h2>{item.title}</h2></Link>
+                {item.length < 300 ?
+                  <p dangerouslySetInnerHTML={{ __html: item.content }}></p> :
+                  <p dangerouslySetInnerHTML={{ __html: item.content.substring(0, 300) }}></p>
+                }
+              </div>
             </div>
-            )
+          )
 
-          })
-          }
+        })
+      }
     </div>
   )
 }
-const mapStateToProps=(state, ownProps) => {
-    if(ownProps.match) {
-        var slug = ownProps.match.params.category_slug;
-    } else{
-        var main_slug = 'film'
-    }
-    
-    return{
-        postCategory:state.posts.posts.filter(cat => {
-            if(slug) return cat.metadata.category.slug ===  slug 
-            else return cat.metadata.category.slug ===  main_slug       
-        })
-    }
+const mapStateToProps = (state, ownProps) => {
+  if (ownProps.match) {
+    var slug = ownProps.match.params.category_slug;
+  } else {
+    var main_slug = 'javascript'
+  }
+
+  return {
+    postCategory: state.posts.posts.filter(cat => {
+      if (slug) return cat.metadata.category.slug === slug
+      else return cat.metadata.category.slug === main_slug
+    })
+  }
 }
 export default connect(mapStateToProps)(Category);
