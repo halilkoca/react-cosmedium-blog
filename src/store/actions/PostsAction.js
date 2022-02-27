dcd66cc4b568150cdef883786ee9e40e994ab1f4
Halil Koca <ihalilkoca@gmail.com>
2022-02-27 13:28:42 +0300

    changed title and lang

--- src/store/actions/PostsAction.js
+++ src/store/actions/PostsAction.js
@@ -1,16 +1,16 @@
 export const FetchPostsApi = () => {
-    return (dispatch) => {
-            const Cosmic = require('cosmicjs')
-            var api = new Cosmic();
-            var bucket = api.bucket({
-              slug: process.env.REACT_APP_COSMIC_BUCKET || 'react-cosmedium-blog',
-              read_key: process.env.REACT_APP_COSMIC_READ_KEY || ''
-            })
-            bucket.getObjects().then(data => {          
-            dispatch({type:"FETCH_POSTS", data:data});            
-            }).catch(err => {
-              console.log(err)
-            })
+  return (dispatch) => {
+    const Cosmic = require('cosmicjs')
+    var api = new Cosmic();
+    var bucket = api.bucket({
+      slug: process.env.REACT_APP_COSMIC_BUCKET || 'react-cosmedium-blog',
+      read_key: process.env.REACT_APP_COSMIC_READ_KEY || ''
+    })
+    bucket.getObjects().then(data => {
+      dispatch({ type: "FETCH_POSTS", data: data });
+    }).catch(err => {
+      console.log(err)
+    })
 
-    }
+  }
 }
